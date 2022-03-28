3c9a9347aea829b4831c28a0cb84e96ac5cdee80
doletu <doletu31@gmail.com>
2022-03-28 15:23:48 +0700

    Add_ToastMessage

---
+++ src/components/ToastMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <Toasts
+    :show-progress="true"
+    :rtl="false"
+    :max-messages="5"
+    :time-out="3000"
+    :closeable="true"
+  ></Toasts>
+</template>
+<script>
+export default {
+  methods: {
+    showSuccessToast() {
+      this.$toastr.s("Success", "Hello world");
+    },
+  },
+};
+</script>
